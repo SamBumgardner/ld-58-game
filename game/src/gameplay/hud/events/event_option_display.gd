82839772a81146787b3d886f9f7aef36c426f009
Sam Bumgardner <Sambumgardner1@gmail.com>
2025-10-05 11:21:12 -0500

    Add: create ui elements for event handling

---
+++ game/src/gameplay/hud/events/event_option_display.gd
@@ -0,0 +1,29 @@
+class_name EventOptionDisplay extends PanelContainer
+
+signal eventOptionSelected(selectedIndex: int);
+
+@export var eventOptionIndex: int = -1;
+
+@onready var title: Label = $%Title;
+@onready var description: Label = $%Description;
+@onready var statCategoryRequirements: Array[Node] = $%StatRequirements.get_children();
+@onready var attemptButton: Button = $%AttemptButton
+
+func _ready() -> void:
+    attemptButton.pressed.connect(_onAttemptButtonPressed);
+
+func setValues(eventOptions: Array[EventOption]) -> void:
+    if eventOptionIndex == -1:
+        push_error("eventOptionDisplay is missing export var setup: see eventOptionSelected");
+
+    var eventOption = eventOptions[eventOptionIndex];
+    title.text = eventOption.title;
+    description.text = eventOption.description;
+    for statCategoryRequirement: StatCategoryRequirements in statCategoryRequirements:
+        statCategoryRequirement.setValues(
+            eventOption.majorStatRequirments,
+            eventOption.minorStatRequirements
+        );
+
+func _onAttemptButtonPressed() -> void:
+    eventOptionSelected.emit(eventOptionIndex);
