fbbad8db9e2dde5025e6ad1c818d9df5d4f54a03
Sam Bumgardner <Sambumgardner1@gmail.com>
2025-10-05 15:29:35 -0500

    Add: implement event results screen.

--- game/src/gameplay/hud/events/event_outcome_display.gd
+++ game/src/gameplay/hud/events/event_outcome_display.gd
@@ -1,23 +1,29 @@
-extends Control
+class_name EventOutcomeDisplay extends Control
 
-signal outcomeDisplayConfirmed(selectedIndex: int);
+signal outcomeDisplayConfirmed();
 
 @onready var title: Label = $%Title;
 @onready var description: Label = $%Description;
 @onready var statCategoryRequirements: Array[Node] = $%StatRequirements.get_children();
 @onready var confirmButton: Button = $%ConfirmButton
-@onready var resultsHeader: Label = $%ResultsHeader
-@onready var resultsContainer: Label = $%ResultsContainer
+@onready var resultsContainer: Control = $%ResultsContainer
+
+@export var testOutcome: MajorOutcome;
+
+func _init() -> void:
+    process_mode = Node.PROCESS_MODE_ALWAYS;
 
 func _ready() -> void:
-    confirmButton.pressed.connect(_onAttemptButtonPressed);
+    confirmButton.pressed.connect(outcomeDisplayConfirmed.emit);
+    
+    if testOutcome != null and self == get_tree().current_scene:
+        open(testOutcome);
 
-func setValues(majorOutcome: MajorOutcome) -> void:
+func open(majorOutcome: MajorOutcome) -> void:
     title.text = majorOutcome.title;
     description.text = majorOutcome.description;
     
     var hasStatChanges: bool = majorOutcome.statChangesToApply != null and not majorOutcome.statChangesToApply.is_empty();
-    resultsHeader.visible = hasStatChanges;
     resultsContainer.visible = hasStatChanges;
     
     if hasStatChanges:
@@ -25,6 +31,7 @@ func setValues(majorOutcome: MajorOutcome) -> void:
             statCategoryRequirement.displayChanges(majorOutcome.statChangesToApply);
     
     confirmButton.text = "Continue" if majorOutcome.ending == null else "Conclude";
+    show();
 
-func _onAttemptButtonPressed() -> void:
-    outcomeDisplayConfirmed.emit();
+func close() -> void:
+    hide();
