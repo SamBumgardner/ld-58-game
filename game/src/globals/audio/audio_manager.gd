85de063301dd6bb07f4add5168a0209b2816cb9c
Noah Bumgardner <noah@playgamecafe.com>
2025-10-05 15:30:16 -0500

    Hook Up Vocal, Always Calls Study

--- game/src/globals/audio/audio_manager.gd
+++ game/src/globals/audio/audio_manager.gd
@@ -1,10 +1,30 @@
 extends Node
 
 @onready var sfxUiClickConfirm = $SFXUIClickConfirm
+@onready var sfxTrainingStudyingComplete = $TrainingComplete/SFXTrainingTypeStudyComplete
 
 func _ready() -> void:
-    EventBus.globalActivitySelected.connect(playSfxUiClickConfirm)
+    EventBus.globalActivitySelected.connect(_playSfxTrainingComplete)
     pass
 
-func playSfxUiClickConfirm(_unused: int) -> void:
+func _playSfxUiClickConfirm(_unused: int) -> void:
     sfxUiClickConfirm.play()
+
+func _playSfxTrainingComplete(_unused: int) -> void:
+    if _unused == 0:
+        _playSfxTrainingTypeStudyComplete()
+    elif _unused == 1:
+        _playSfxTrainingTypePhysicalComplete()
+    elif _unused == 2:
+        _playSfxTrainingTypeCraftComplete()
+    else:
+        print_debug("Warning: Tried to play SFX out of bounds at", _unused)
+
+func _playSfxTrainingTypeStudyComplete() -> void:
+    sfxTrainingStudyingComplete.play()
+
+func _playSfxTrainingTypePhysicalComplete() -> void:
+    sfxTrainingStudyingComplete.play()
+
+func _playSfxTrainingTypeCraftComplete() -> void:
+    sfxTrainingStudyingComplete.play()
