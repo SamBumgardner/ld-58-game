585031d2ad228a4637d51bab7b0954917bd12d65
Sam Bumgardner <Sambumgardner1@gmail.com>
2025-10-06 00:15:31 -0500

    Add: icon mood and weather UI

--- game/src/gameplay/hud/weather_display.gd
+++ game/src/gameplay/hud/weather_display.gd
@@ -1,12 +1,14 @@
 extends Control
 
 @onready
-var weatherDisplayLabel: Label = $CurrentWeather;
+var weatherDisplayLabel: Label = $CurrentWeatherText;
 @onready
-var forecastDisplayLabel: Label = $ForecastWeather;
+var forecastDisplayLabel: Label = $ForecastWeatherText;
 
 func _onWeatherChange(newWeather: Weather.Types) -> void:
     weatherDisplayLabel.text = "Current Weather: %s" % Weather.Types.find_key(newWeather);
+    $CurrentWeather.texture = load(Weather.WeatherIconPaths[newWeather]);
 
 func _onForecastChange(newForecast: Weather.Types) -> void:
     forecastDisplayLabel.text = "Tomorrow's Weather: %s" % Weather.Types.find_key(newForecast);
+    $ForecastWeather.texture = load(Weather.WeatherIconPaths[newForecast]);
