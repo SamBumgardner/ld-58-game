48daaad77770fe7d3cc99caa570f8c51e54bdd63
Noah Bumgardner <noah@playgamecafe.com>
2025-10-05 22:29:18 -0500

    Define Start Menu, Hook Up Play and Quit
    Add themes to control sizes.
    Hook up sounds only for other buttons.

--- game/src/globals/event_bus.gd
+++ game/src/globals/event_bus.gd
@@ -2,3 +2,9 @@ extends Node
 
 @warning_ignore("unused_signal")
 signal globalActivitySelected(activityIndex: int);
+
+@warning_ignore("unused_signal")
+signal globalUiElementMouseEntered();
+
+@warning_ignore("unused_signal")
+signal globalUiElementSelected();
---
+++ game/src/menus/start/start_menu.gd
@@ -0,0 +1,46 @@
+extends Control
+
+#region node init
+@onready var buttonQuit: Button = $%ButtonQuit;
+#endregion
+
+func _ready() -> void:
+    if OS.get_name() == "Web":
+        buttonQuit.visible = false
+
+#region button mouse entered
+func _on_button_credits_mouse_entered():
+    EventBus.globalUiElementMouseEntered.emit()
+
+func _on_button_play_mouse_entered():
+    EventBus.globalUiElementMouseEntered.emit()
+
+func _on_button_quit_mouse_entered():
+    EventBus.globalUiElementMouseEntered.emit()
+
+func _on_button_settings_mouse_entered():
+    EventBus.globalUiElementMouseEntered.emit()
+#endregion
+
+#region button pressed
+func _on_button_credits_pressed():
+    EventBus.globalUiElementSelected.emit()
+    pass
+
+func _on_button_play_pressed():
+    EventBus.globalUiElementSelected.emit()
+    get_tree().change_scene_to_file("res://src/gameplay/gameplay.tscn")
+
+func _on_button_quit_pressed():
+    EventBus.globalUiElementSelected.emit()
+    get_tree().quit()
+
+func _on_button_settings_pressed():
+    EventBus.globalUiElementSelected.emit()
+    pass
+#endregion
+
+#region menu bar mouse entered
+func _on_menu_bar_choose_character_mouse_entered():
+    EventBus.globalUiElementMouseEntered.emit()
+#endregion
