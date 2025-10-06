ceb1f2c1a7fe0ff2cad93d7eecdd97f0cb4d4cee
Noah Bumgardner <noah@playgamecafe.com>
2025-10-05 23:18:33 -0500

    Implement Credits Screen

--- game/src/menus/start/start_menu_content.gd
+++ game/src/menus/start/start_menu_content.gd
@@ -25,7 +25,7 @@ func _on_button_settings_mouse_entered():
 #region button pressed
 func _on_button_credits_pressed():
     EventBus.globalUiElementSelected.emit()
-    pass
+    get_tree().change_scene_to_file("res://src/menus/credits/credits_menu.tscn")
 
 func _on_button_play_pressed():
     EventBus.globalUiElementSelected.emit()
